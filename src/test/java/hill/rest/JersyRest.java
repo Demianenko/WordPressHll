991f4fd8b7287f162da40200dedf895fdbe26c66
Demianenko <erlond@narod.ru>
2016-11-03 15:47:55 +0200

    d

--- src/test/java/hill/rest/JersyRest.java
+++ src/test/java/hill/rest/JersyRest.java
@@ -42,5 +42,30 @@ public class JersyRest extends JerseyTest {
         Assert.assertEquals(actual,"{\"lon\":-0.13,\"lat\":51.51}");
     }
 
+    @Test(enabled=true)
+    public void test2() throws JSONException {
+        String url = "http://gci01-p01-jws01.lab.nordigy.ru";
+        String puth = "/phonedevices/hp_returns.asp";
 
+        String p1 ="PO";
+        String v1 = "080304153247|411274568004";
+        String p2 ="SN";
+        String v2 ="32132132";
+        String p3 ="LN";
+        String v3 ="803651132004";
+        String p4 = "Device_Condition";
+        String v4 ="1";
+        String p5 = "Condition_Note";
+        String v6 = "Test+Note";
+        WebResource webResource = client().resource(url);
+        JSONObject json = webResource.path(puth)
+                .queryParam(p1,v1)
+                .queryParam(p2,v2)
+                .queryParam(p3,v3)
+                .queryParam(p4,v4)
+                .get(JSONObject.class);
+        String actual = json.get("").toString();
+        System.out.println(json);
+
+    }
 }
